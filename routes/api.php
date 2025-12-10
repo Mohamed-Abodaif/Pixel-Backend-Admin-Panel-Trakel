459fd83a18e6e275303c59afe7b3107e81e7f6a3
Muhammed ALDRUBI <MuhammedALDRUBI>
2025-12-10 11:34:19 +0200

    change in routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\CompanyModule\TenantCompany;
+use App\Models\UsersModule\User;
+use App\Services\CompanyModule\StatusChangerServices\CompanyTypeStatusChangers\SignUpCompanyStatusChangerServices\SignUpAccountApprovingService;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use PixelApp\Helpers\PixelGlobalHelpers;
@@ -10,7 +12,31 @@ PixelGlobalHelpers::requirePhpFiles(__DIR__ . '/CompanyModule');
 Route::get("test" , function()
 {
     
+$user = new User();
+
+$user->name               = "first admin";
+$user->email              = "admin@pixel.com";
+$user->password           = Hash::make(123456789);
+$user->first_name         = "first";
+$user->last_name          = "admin";
+$user->mobile             = "224324";
+$user->hashed_id          = "4fsd4-4aa4-42cc-rter";
+$user->user_type          = "user";
+$user->email_verified_at  = now();
+$user->accepted_at        = now();
+$user->department_id      = 1;
+$user->status             = "active";
+$user->default_user       = 1;
+$user->role_id            = 1;
+$user->branch_id          = 1;
+
+$user->save();
+
     $company = TenantCompany::first();
+    
+
+    (new SignUpAccountApprovingService(1))->approve();
+
     return $company;
 
     return response()->json([
